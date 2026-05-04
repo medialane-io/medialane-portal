88f220e3142cc3e46282509c53717ce7ca74c0ec
salvador camino <salvadorcamino@gmail.com>
2026-05-04 09:45:42 -0300

    chore: remove Better Auth, ChipiPay; add starknet + jose

--- src/lib/auth-client.ts
+++
@@ -1,9 +0,0 @@
-import { createAuthClient } from "better-auth/react";
-import { jwtClient } from "better-auth/client/plugins";
-
-export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_APP_URL ?? "https://portal.medialane.io",
-  plugins: [jwtClient()],
-});
-
-export type Session = typeof authClient.$Infer.Session;
